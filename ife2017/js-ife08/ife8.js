a56d23065213dfa8e0db4e79d1d4f1d951398c4b
changemelee@gmail.com <changemelee@gmail.com>
2017-05-10 23:21:02 +0800

    add ife8

---
+++ ife2017/js-ife08/ife8.js
@@ -0,0 +1,27 @@
+function $ (id) {
+  return document.getElementById(id);
+}
+var data = [];
+
+function render (value) {
+    data.forEach(function (item) {
+      if(value != null   && value.length !== 0) {
+        item.replace(new RegExp(value,"g"),`<span id="selected">${value}</span>`);
+      }
+      $("show").innerHTML += `<div id="items">${item}</div>`;
+  });
+}
+  
+
+$("insert").onclick = function() {
+  var str = $("textarea").value.trim();
+  data = str.split(/[^0-9a-z-A-Z\u4e00-\u9fa5]+/)
+            .filter(function (item) {
+               return (item.length !== 0);
+            });
+  render();
+};
+$("search").onclick = function() {
+  var searchValue = $("searchInput").value.trim();
+  render(searchValue);
+};
