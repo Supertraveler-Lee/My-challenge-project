7c6c7ca615be7fc23f00e6dc2b2f373e29a2433a
changemelee@gmail.com <changemelee@gmail.com>
2017-05-24 23:46:28 +0800

    update ife11

--- ife2017/js-ife11/ife11.js
+++ ife2017/js-ife11/ife11.js
@@ -1,82 +1,116 @@
-var timer= null,
+var timer = null,
+    lock = false,
     btns = document.getElementsByTagName("button"),
+    input = document.getElementsByTagName("input")[0],
     rootNode = document.getElementById("root"),
-    indexBF = 0 ;  //广度自增标识
+    indexBF = 0; //广度自增标识
 
-function addEvent(elem,event,handler) {
+function addEvent(elem, event, handler) {
   if (elem.addEventListeren) {
-    elem.addEventListeren(event,handler,false);
+    elem.addEventListeren(event, handler, false);
   } else if (elem.attachEvent) {
-    elem.attachEvent("on"+event,handler);
+    elem.attachEvent("on" + event, handler);
   } else {
-    elem["on"+event] = handler;
+    elem["on" + event] = handler;
   }
 }
 
-function breadthFirst(node,nodeList) {
+function breadthFirst(node, nodeList) {
   if (node) {
     nodeList.push(node);
-    breadthFirst(node.nextElementSibling,nodeList);
+    breadthFirst(node.nextElementSibling, nodeList);
     node = nodeList[indexBF++];
-    breadthFirst(node.firstElementChild,nodeList);
+    breadthFirst(node.firstElementChild, nodeList);
   }
   return nodeList;
 }
 
 
-function depthFirst(node,nodeList) {
+function depthFirst(node, nodeList) {
   if (node) {
     nodeList.push(node);
-    for(var i=0, len=node.children.length; i<len; i++ ) {
-      depthFirst(node.children[i],nodeList);
+    for (var i = 0, len = node.children.length; i < len; i++) {
+      depthFirst(node.children[i], nodeList);
     }
   }
-  alert(nodeList);
   return nodeList;
 }
 
-function render(nodeList) {
+function render(nodeList,value) {
+  value = value || 0;
   var len = nodeList.length,
-      i = 1;
-  
+    i = 1;
+  lock = true;
   rootNode.className = "active";
-  timer = setInterval(function () {
-    if (i<len) {
-      nodeList[i-1].className = "";
-      nodeList[i].className = "active";
+  timer = setInterval(function() {
+    if (i < len) {
+      console.log(nodeList[i-1]);
+      console.log(nodeList[i-1].data);
+      if (value.toLowerCase() !== nodeList[i-1].nodeValue.toLowerCase()) {
+        nodeList[i - 1].className = "";
+        nodeList[i].className = "active";
+      } else {
+        nodeList[i-1].className = "select"
+        clearInterval(timer);
+        lock = false;
+      }
+      
     } else {
       clearInterval(timer);
+      nodeList[len - 1].className = "default";
+      lock = false;
     }
     i++;
-  },500);
+  }, 500);
+  
 }
 
 function reset() {
   divsNodes = document.getElementsByTagName("div");
   divsLen = divsNodes.length;
-  for(var i=0; i<divsLen; i++) {
+  for (var i = 0; i < divsLen; i++) {
     divsNodes[i].className = "default";
   }
 }
 
-addEvent(btns[0], "click", function () {
-  var nodeList = [];
-  reset();
-  depthFirst(rootNode,nodeList);
-  render(nodeList);
-});
-
-for (var i=0; i<2;i++) {
-  addEvent(btns[i], "click", function () {
-    var nodeList = [];
-      reset();
-      
-      render(nodeList);
-    });
-
- };
-
-
-function traversal() {
-  reset();
+function buttons(index,nodeList) {
+  var button = {
+    "0": function() {
+      depthFirst(rootNode, nodeList);
+    },
+    "1": function() {
+      breadthFirst(rootNode, nodeList);
+    },
+    "2" : function() {
+      depthFirst(rootNode, nodeList);
+    },
+    "3" : function() {
+      breadthFirst(rootNode, nodeList);
+    },
+  };
+  if (typeof button[index] !== "function") {
+    throw new Error("invalid index");
+  }
+  return button[index]();
 }
+
+function init () {
+  for (var i = 0; i < btns.length; i++) {
+    var nodeList= [];
+    (function(i) {
+      addEvent(btns[i], "click", function() {
+        console.log(lock);
+        if (lock === true) {
+          alert("正在遍历，请稍等...");
+        } else {
+          value = input.value;
+          reset();
+          buttons(i,nodeList);
+          render(nodeList,value);
+          nodeList = [];
+        }        
+      });
+    }(i));
+  };
+}
+init();
