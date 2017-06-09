ed935961a0eb81d1911ff774d74dc2177c2a4bf2
changemelee@gmail.com <changemelee@gmail.com>
2017-06-09 23:38:52 +0800

    update ife14

--- ife2017/js-ife14/ife14.js
+++ ife2017/js-ife14/ife14.js
@@ -1,40 +1,93 @@
-
-var width = 100;
-var height = 30;
-var z = 120;
-
-
-// var elem = document.getElementById("space1");
-// elem.style.top = (Math.round(y))+'px';
-// elem.style.left = (Math.round(x))+'px';
-
-var elem = document.getElementById("space1");
-// elem.style.top = -15;
-// elem.style.left = 65;
-function ele() {
-  this.deg = 0;
-
-  elem.style.top = -15+'px';
-  var top = -15;
-  var left = 65;
-  elem.style.left = 222/2-100/2+'px';
-  var timer = setInterval(function () {
-   this.deg += 1; 
-    var x = Math.sin(this.deg*2*Math.PI/360)*z;
-    var y = Math.cos(this.deg*2*Math.PI/360)*z*(-1);
-   elem.style.transform = 'rotate('+this.deg+'deg)';
-   x += 122 - 15;
-   y+= 122 - 50;
-   elem.style.top = -15+x+'px';
-   elem.style.left = 65+y+'px';
-   console.log(x,y);
-   console.log(elem.style.top);
-   console.log(elem.style.left);
-
-   if (this.deg >360) {
-    clearInterval(timer);
-   }
-   console.log(this.deg);
-  },100);
+function addEvent(elem, event, handler) {
+  if (elem.addEventListener) {
+    elem.addEventListener(event, handler, false);
+  } else if (elem.attachEvent) {
+    elem.attachEvent("on" + event, handler);
+  } else {
+    elem["on" + event] = handler;
+  }
 }
-ele();
+
+function getId(id) {
+  return document.getElementById(id);
+}
+
+function getClass(className) {
+  return document.getElementsByClassName(className);
+}
+
+var Spaceship = function() {
+  var fly = 0;
+  var launch = 0;
+};
+
+Spaceship.prototype.fly = function() {
+
+};
+Spaceship.prototype.stop = function() {
+
+};
+Spaceship.prototype.destroy = function() {
+
+};
+Spaceship.prototype.animation = function() {
+
+};
+
+
+
+var Commander = function() {
+  this.cmd = {};
+};
+
+Commander.prototype.init = function() {
+  var i = 1;
+  var buttonCmd = [];
+  addEvent(getId("createspace"), "click", function() {
+    ++i;
+    if (i < 5) {
+      var newspace = document.createElement("div");
+      newspace.className = "single-control";
+      newspace.id = "control" + i;
+      newspace.innerHTML = "<p class='spacenumber'>飞船" + i + "号</p> \
+                          <button class='tostart'>开始飞行</button>\
+                          <button class='tostop'>停止飞行</button>\
+                          <button class='destryspace'>摧毁飞船</button>";
+      getId("console").appendChild(newspace);
+      buttonCmd = [];
+      addButtonEvent();
+    } else {
+      alert("只有4条轨道 ：(");
+    }
+  });
+  var addButtonEvent = function() {
+    Array.prototype.forEach.call(getId('control1').getElementsByTagName("button"), function(item) {
+      buttonCmd.push(item.className);
+    });
+    buttonCmd.map(function(item) {
+      Array.prototype.forEach.call(getClass(item), function(elem, index) {
+        addEvent(elem, "click", function() {
+          this.cmd = {
+            id: index + 1,
+            cmd: item + (index + 1)
+          };
+          console.log(this.cmd);
+          Commander.Mediator();
+        });
+      });
+    });
+  };
+  addButtonEvent();
+};
+
+Commander.prototype.Mediator = function() {
+  var errRate = 0.3;
+  if (Math.random() < errRate) {
+    getId('log').innerHTML = "发送的无线电信号未收到...";
+    return ;
+  };
+      
+};
+
+Commander = new Commander();
+Commander.init();
