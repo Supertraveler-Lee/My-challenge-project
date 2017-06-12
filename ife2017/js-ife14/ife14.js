45904202c849c9e6c56b3dce926f07123efa3aa8
changemelee@gmail.com <changemelee@gmail.com>
2017-06-12 23:38:35 +0800

    improve ife14

--- ife2017/js-ife14/ife14.js
+++ ife2017/js-ife14/ife14.js
@@ -15,26 +15,9 @@ function getId(id) {
 function getClass(className) {
   return document.getElementsByClassName(className);
 }
-
-var Spaceship = function() {
-  var fly = 0;
-  var launch = 0;
-};
-
-Spaceship.prototype.fly = function() {
-
-};
-Spaceship.prototype.stop = function() {
-
-};
-Spaceship.prototype.destroy = function() {
-
-};
-Spaceship.prototype.animation = function() {
-
-};
-
-
+function getElemTarget(id,className) {
+  return getId('control'+id).getElementsByClassName(className)[0];
+}
 
 var Commander = function() {
   this.cmd = {};
@@ -65,14 +48,15 @@ Commander.prototype.init = function() {
       buttonCmd.push(item.className);
     });
     buttonCmd.map(function(item) {
-      Array.prototype.forEach.call(getClass(item), function(elem, index) {
+      var buttonClass = getClass(item);
+      Array.prototype.forEach.call(buttonClass, function(elem, index) {
         addEvent(elem, "click", function() {
-          this.cmd = {
+          Commander.cmd = {
             id: index + 1,
-            cmd: item + (index + 1)
+            handler: item
           };
-          console.log(this.cmd);
-          Commander.Mediator();
+          console.log(Commander.cmd);
+          Mediator.deliver(buttonClass, Commander.cmd);
         });
       });
     });
@@ -80,14 +64,63 @@ Commander.prototype.init = function() {
   addButtonEvent();
 };
 
-Commander.prototype.Mediator = function() {
-  var errRate = 0.3;
-  if (Math.random() < errRate) {
+var Mediator = function() {
+
+};
+
+Mediator.prototype.deliver = function(buttonClass, cmd) {
+  this.errRate = 0.3;
+  if (Math.random() < this.errRate) {
     getId('log').innerHTML = "发送的无线电信号未收到...";
-    return ;
-  };
-      
+    return;
+  }
+  Array.prototype.forEach.call(buttonClass, function(item, index) {
+    if (cmd.id === (index + 1)) {
+      console.log('xxx'+cmd.id);
+      var target = getElemTarget(index+1,item.className);
+      Spacecraft[cmd.handler](target);
+    }   
+  });
+};
+
+var Spacecraft = function() {
+
+};
+
+Spacecraft.prototype.tostop = function(target) {
+  console.log("please  stop!");
+};
+Spacecraft.prototype.tostart = function(target) {
+  console.log("please  start!");
+  console.log(target);
+  Spacecraft.animation(target);
+
+};
+Spacecraft.prototype.destryspace = function(target) {
+  console.log("please  destryspace!");
+};
+Spacecraft.prototype.getLocal = function() {
+  var PLANET_RADIUS = 111;
+  var ORBIT_ALTITUDE = 61;
+  var Spacecraft_WIDTH = 100;
+  var Spacecraft_HEIGHT = 30;
+  var z = Spacecraft_WIDTH + Spacecraft_HEIGHT;
+  var x = Math.sin(Math.PI * 2 * this.degree) * z;
+  var y = Math.cos(Math.PI * 2 * this.degree) * -z;
+  x += PLANET_RADIUS - Spacecraft_WIDTH;
+  y += PLANET_RADIUS - Spacecraft_HEIGHT;
+  console.log(x,y);
+  return [x,y];
+};
+Spacecraft.prototype.animation = function(target) {
+  this.degree = 0;
+  target.style.transform = 'rotate('+ this.degree + "deg)";
+  var loc = this.getLocal();
+  target.style.left = loc[0] + 'px';
+  target.style.right = loc[1] + 'px';
 };
 
 Commander = new Commander();
+Mediator = new Mediator();
+Spacecraft = new Spacecraft();
 Commander.init();
