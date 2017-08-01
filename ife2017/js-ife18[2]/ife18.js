b0596865c11726042769976a718ed975b1e2015b
changemelee@gmail.com <changemelee@gmail.com>
2017-08-01 23:41:32 +0800

    update ife18[2]

---
+++ ife2017/js-ife18[2]/ife18.js
@@ -0,0 +1,15 @@
+//方案二 采用坐标形式
+var command = {
+  X: 5,
+  Y: 5
+};
+var path = ['left','top','right','bottom'];
+function getId(id) {
+  return document.getElementById(id);
+}
+function resetDiv() {
+  getId('selected').innerHTML = "";
+}
+function setLocate(X,Y) {
+  
+}
