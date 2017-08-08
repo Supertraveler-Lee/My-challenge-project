8e77672df5fc8626dc84f53cb9076e7c83c1e25e
changemelee@gmail.com <changemelee@gmail.com>
2017-08-08 23:48:25 +0800

    update ife19

--- ife2017/js-ife19/ife19.js
+++ ife2017/js-ife19/ife19.js
@@ -1,30 +1,34 @@
 var command = {
-  statusNum: 0,
-  status: "left",
+  // statusNum: 0,
+  deg: 0,
+  status: "LEF",
   X: 5,
   Y: 5
 };
-var path = ['left', 'top', 'right', 'bottom'];
+var path = ['LEF', 'TOP', 'RIG', 'BOT', "BACK"];
+var action = ['TURN ', 'TRA ', 'MOVE '];
 
 function getId(id) {
   return document.getElementById(id);
 }
-function animate(deg) {
-  var timer = setInterval(frame,300);
+
+function animate(eachDeg,callback) {
+  var timer = setInterval(frame, 300);
   var id = 1;
-  var everyDeg = 0;
+
   function frame() {
-    // var selected = getId('selected').style.transform;
-    // console.log(getId('selected').style.transform);
-    if(id>3) {
+    if (id > 3) {
+      command.status = path[command.deg/90];
       clearInterval(timer);
+      callback();
     } else {
-      everyDeg += deg/3;
-      getId('selected').style.transform = "rotate("+everyDeg+"deg)";
+      command.deg += eachDeg / 3;
+      getId('selected').style.transform = "rotate(" + command.deg + "deg)";
       id++;
     }
   }
 }
+
 function trim() {
   if (!String.prototype.trim) {
     var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
@@ -33,60 +37,111 @@ function trim() {
     };
   }
 }
+
 function getDiv() {
   var horizontal = document.getElementsByTagName('tr')[command.Y];
   var vertical = horizontal.getElementsByTagName('td')[command.X];
   return vertical;
 }
-function movesquare() {
-  var goal = getDiv();
-  command.status = path.slice(command.statusNum % 4)[0];
-  switch (command.status) {
-    case 'left':
+
+function checkBoard() {
+  if (command.X > 10 || command.X <= 0 || command.Y > 10 || command.Y <= 0) {
+    alert('已经到边界了');
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function move1Unit(index, value) {
+  var goalDiv = getDiv();
+  var tempdeg = [command.X, command.Y];
+  switch (value) {
+    case action[index] + path[0]:
       command.X -= 1;
       break;
-    case 'right':
+    case action[index] + path[1]:
+      command.Y -= 1;
+      break;
+    case action[index] + path[2]:
       command.X += 1;
       break;
-    case 'top':
-      command.Y -= 1;
-    case 'bottom':
+    case action[index] + path[3]:
       command.Y += 1;
       break;
   }
-  if (command.X > 10 || command.X <= 0 || command.Y > 10 || command.Y <= 0) {
-    alert('已经到边界了');
+  if (checkBoard()) {
+    command.X = tempdeg[0];
+    command.Y = tempdeg[1];
     return false;
-  } 
-  goal.innerHTML = "";
-  goal.removeAttribute('id');
-  var goal2 = getDiv();
-  goal2.innerHTML = "<div></div>";
-  goal2.id = 'selected';
+  }
+  goalDiv.innerHTML = "";
+  goalDiv.removeAttribute('id');
+  var goalDiv2 = getDiv();
+  goalDiv2.innerHTML = "<div></div>";
+  goalDiv2.style.transform = "rotate("+command.deg+"deg)";
+  goalDiv2.id = 'selected';
+}
+
+function turnSquare(value) {
+  switch (value) {
+    case action[0] + path[0]:
+      command.statusNum -= 1;
+      animate(-90);
+      break;
+    case action[0] + path[2]:
+      command.statusNum += 1;
+      animate(90);
+      break;
+    case action[0] + path[4]:
+      command.statusNum += 2;
+      animate(180);
+      break;
+    case 'GO':
+  // command.deg = path.slice(command.statusNum % 4)[0];
+      command.status = path[((command.deg % 360 + 360) % 360)/90];
+      move1Unit(0, action[0] + command.status);
+  }
+}
+
+function traSquare(value) {
+  move1Unit(1, value);
+}
+
+function moveSquare(value) {
+  var diff;
+  command.deg = (command.deg % 360 + 360) % 360;
+  if(action[2]+command.status !== value){
+    switch (value) {
+    case action[2] + path[0]:
+      diff = 0 - command.deg;
+      break;
+    case action[2] + path[1]:
+      diff = 90 - command.deg;
+      break;
+    case action[2] + path[2]:
+      diff = 180 - command.deg;
+      break;
+    case action[2] + path[3]:
+      diff = 270 - command.deg;
+      break;
+    }
+    animate(diff,function(){move1Unit(2,value)});
+  } else {
+  move1Unit(2,value);
+  }
+  
 }
 
 function run() {
   var value = document.querySelectorAll('input[type=text]')[0].value.trim();
-  switch (value) {
-    case 'TURN LEF':
-      command.statusNum -= 1;
-      deg -= 90;
-      animate(deg);
-      break;
-    case 'TURN RIG':
-      command.statusNum += 1;
-      deg += 90;
-      animate(deg);
-      break;
-    case 'TURN BACK':
-      deg += 180;
-      command.statusNum += 2;
-      animate(deg);
-      break;
-    case 'GO':
-      movesquare();
+  if (value.indexOf(action[0]) !== -1 || value === "GO") {
+    turnSquare(value);
+  } else if (value.indexOf(action[1]) !== -1) {
+    traSquare(value);
+  } else if (value.indexOf(action[2]) !== -1) {
+    moveSquare(value);
   }
-  // getId("selected").style.transform = 'rotate(' + deg + 'deg)';
 }
 
 function addEvent(e, event, listener) {
@@ -98,7 +153,6 @@ function addEvent(e, event, listener) {
     e["on" + event] = listener;
   }
 }
-var deg = 0;
 addEvent(document.querySelectorAll('input[type=submit]')[0], "click", function(e) {
   run();
 });
